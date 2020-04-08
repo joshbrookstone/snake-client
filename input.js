60edcb2380ac1f6ec5ed2e5ecc2c650c49e553c0
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-08 14:24:00 -0700

    added words
    Co-authored-by: Ali Yazdani <aliyazdani82@gmail.com>

--- input.js
+++ input.js
@@ -1,16 +1,42 @@
+let connection;
+let interval;
+let currCommand;
+
+const runCommand = function(command) {
+  if (currCommand !== command) {
+    currCommand = command;
+  } else {
+    return;
+  }
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    connection.write(command);
+  }, 100);
+};
+
 const handleUserInput = function(data) {
-  console.log(data);
-  // switch (data) {
-  //   case ("\u0003"):
-  //     process.exit()
-  //     break;
- 
+  
   if (data === "\u0003") {
     process.exit();
+  } else if (data === "\u0077") {
+    runCommand("Move: up");
+  } else if (data === "\u0061") {
+    runCommand("Move: left");
+  } else if (data === "\u0073") {
+    runCommand("Move: down");
+  } else if (data === "\u0064") {
+    runCommand("Move: right");
+  } else if (data === "\u0070") {
+    connection.write("Say: yoyo");
+  } else if (data === "\u006F") {
+    connection.write("Say: lol :p");
   }
 };
 
-const setUpInput = function() {
+const setUpInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
--- play.js
+++ play.js
@@ -2,6 +2,7 @@ const { connect } = require('./client');
 const { setUpInput } = require("./input");
 
 console.log('Connecting ...');
-connect();
 
-setUpInput();
+setUpInput(connect());
+
+// Update play.js to send the connection object returned from connect() into our setupInput() function.
